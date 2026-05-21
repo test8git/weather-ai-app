4dcd945270faa60f2abd5b975c851702d863effa
Colonel Software <test8cs@gmail.com>
2026-05-21 14:43:44 +0530

    Remove static export

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export',
 };
 
 export default nextConfig;
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
       }
 
       const res = await fetch(
-        `http://127.0.0.1:8000/weather?city=${city}`
+        `${process.env.NEXT_PUBLIC_API_URL}/weather?city=${city}`
       );
 
       if (!res.ok) {
